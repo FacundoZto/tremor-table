65889c22f9bfae4be5fd7d68b44f01f4235f1d62
facundozto <facundozerbato@outlook.com>
2024-12-04 16:12:08 -0300

    adding redux

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -8,111 +8,44 @@ import {
   TableRoot,
   TableRow,
 } from "./Table";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 export function UserTable() {
-  const data: Array<{
-    workspace: string;
-    owner: string;
-    status: string;
-    costs: string;
-    region: string;
-    capacity: string;
-    lastEdited: string;
-  }> = [
-    {
-      workspace: "sales_by_day_api",
-      owner: "John Doe",
-      status: "Live",
-      costs: "$3,509.00",
-      region: "US-West 1",
-      capacity: "99%",
-      lastEdited: "23/09/2023 13:00",
-    },
-    {
-      workspace: "marketing_campaign",
-      owner: "Jane Smith",
-      status: "Live",
-      costs: "$5,720.00",
-      region: "US-East 2",
-      capacity: "80%",
-      lastEdited: "22/09/2023 10:45",
-    },
-    {
-      workspace: "test_environment",
-      owner: "David Clark",
-      status: "Inactive",
-      costs: "$800.00",
-      region: "EU-Central 1",
-      capacity: "40%",
-      lastEdited: "25/09/2023 16:20",
-    },
-    {
-      workspace: "sales_campaign",
-      owner: "Jane Smith",
-      status: "Live",
-      costs: "$5,720.00",
-      region: "US-East 2",
-      capacity: "80%",
-      lastEdited: "22/09/2023 10:45",
-    },
-    {
-      workspace: "development_env",
-      owner: "Mike Johnson",
-      status: "Inactive",
-      costs: "$4,200.00",
-      region: "EU-West 1",
-      capacity: "60%",
-      lastEdited: "21/09/2023 14:30",
-    },
-    {
-      workspace: "new_workspace_1",
-      owner: "Alice Brown",
-      status: "Inactive",
-      costs: "$2,100.00",
-      region: "US-West 2",
-      capacity: "75%",
-      lastEdited: "24/09/2023 09:15",
-    },
-  ];
+  const users = useSelector((state: RootState) => state.users);
 
   return (
-    <>
-      <div className="p-10">
-        <h3 className="font-semibold text-gray-900 dark:text-gray-50">
-          Workspaces
+    <div className="w-full">
+      <div className="mt-9 ml-8 flex">
+        <h3 className="font-semibold text-gray-900 dark:text-gray-50 mr-3">
+          Users 
         </h3>
-        <p className="mt-1 text-sm leading-6 text-gray-600 dark:text-gray-400">
-          Overview of all registered workspaces within your organization.
-        </p>
+        <Badge variant="default">{users.length}</Badge>
       </div>
-      <TableRoot className="mt-8 p-11">
+      <TableRoot className="mt-2 p-11">
         <Table>
           <TableHead>
             <TableRow>
-              <TableHeaderCell>Workspace</TableHeaderCell>
-              <TableHeaderCell>Owner</TableHeaderCell>
-              <TableHeaderCell>Status</TableHeaderCell>
-              <TableHeaderCell>Region</TableHeaderCell>
-              <TableHeaderCell>Capacity</TableHeaderCell>
-              <TableHeaderCell className="text-right">Costs</TableHeaderCell>
+              <TableHeaderCell>Picture</TableHeaderCell>
+              <TableHeaderCell>Name</TableHeaderCell>
+              <TableHeaderCell>Gender</TableHeaderCell>
+              <TableHeaderCell>Country</TableHeaderCell>
+              <TableHeaderCell>City</TableHeaderCell>
+              <TableHeaderCell>E-mail</TableHeaderCell>
               <TableHeaderCell className="text-right">Actions</TableHeaderCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((item) => (
-              <TableRow key={item.workspace}>
-                <TableCell>{item.workspace}</TableCell>
-                <TableCell>{item.owner}</TableCell>
+            {users.map((item) => (
+              <TableRow key={item.id.value}>
                 <TableCell>
-                  <Badge
-                    variant={item.status === "Inactive" ? "warning" : "default"}
-                  >
-                    {item.status}
-                  </Badge>
+                  <img src={item.picture.thumbnail} alt="User" />
                 </TableCell>
-                <TableCell>{item.region}</TableCell>
-                <TableCell>{item.capacity}</TableCell>
-                <TableCell className="text-right">{item.costs}</TableCell>
+                <TableCell>{item.name.first}</TableCell>
+                <TableCell>{item.gender}</TableCell>
+                <TableCell>{item.location.country}</TableCell>
+                <TableCell>{item.location.city}</TableCell>
+                <TableCell>{item.email}</TableCell>
                 <TableCell className="text-right">
                   <button type="button">
                     <svg
@@ -152,6 +85,6 @@ export function UserTable() {
           </TableBody>
         </Table>
       </TableRoot>
-    </>
+    </div>
   );
 }
---
+++ src/store/slices/users.ts
@@ -0,0 +1,57 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { User } from "../../interfaces/User";
+
+const initialState: User[] = [
+  {
+    id: {
+      name: "SSN",
+      value: "405-88-3636",
+    },
+    name: {
+      title: "Miss",
+      first: "Jane",
+      last: "Smith",
+    },
+    gender: "female",
+    email: "jennie.nichols@example.com",
+    location: {
+      city: "Billings",
+      state: "Michigan",
+      country: "United States",
+    },
+    picture: {
+      medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+    }
+  },
+  {
+    id: {
+      name: "SDN",
+      value: "405-04-3636",
+    },
+    name: {
+      title: "Miss",
+      first: "Prueba",
+      last: "Smith",
+    },
+    gender: "female",
+    email: "judith.nichols@example.com",
+    location: {
+      city: "Madrid",
+      state: "Madrid",
+      country: "España",
+    },
+    picture: {
+      medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+    }
+  },
+];
+
+const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+});
+
+export default userSlice.reducer;
